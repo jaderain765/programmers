3b1aeac8978d587faa70202804a0b728bb78e2be
Jade_Rain <83251008+jaderain765@users.noreply.github.com>
2022-06-17 17:54:18 +0900

    Update 헤쉬셋.java

---
+++ 알고리즘_문법/헤쉬셋.java
@@ -0,0 +1,21 @@
+package 알고리즘_문법;
+
+import java.util.*;
+
+public class 헤쉬셋 {
+    public static void main(String[] args) {
+        
+        HashSet<Integer> hs = new HashSet<>();
+        /* 
+         * 일반 적으로 사용되는 HashSet이다.
+         * 담을 객체에 따라서 <>내부의 값을 조정한다.
+         * 문자를 넣고 싶을시 Character
+        */
+        
+        HashSet<Integer> hs = new HashSet<Integer>(20);
+        
+
+
+
+    }
+}
